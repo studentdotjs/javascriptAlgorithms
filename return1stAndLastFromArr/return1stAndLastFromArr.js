8027adcc612e5831038ed1398d5e6154b5b0001f
Naveen Sharma <naveencoder13@gmail.com>
2019-05-19 11:10:35 +0530

    Added square brackets in the variables of "Return the First and Last Elements in an New Array" javascript algorithm file.

--- return1stAndLastFromArr/return1stAndLastFromArr.js
+++ return1stAndLastFromArr/return1stAndLastFromArr.js
@@ -1,9 +1,9 @@
 function returnFirstAndLastElement(arr) {
-    console.log(`Old Array : ${arr}.`);
+    console.log(`Old Array : [${arr}].`);
     let newArr = [];
     newArr[0] = arr.shift();
     newArr[1] = arr.pop();
-    console.log(`New Array : ${newArr}.`);
+    console.log(`New Array : [${newArr}].`);
 }
 
 returnFirstAndLastElement(['naveen', 22, true, 'jaipur', 'rajasthan', 'india']);
