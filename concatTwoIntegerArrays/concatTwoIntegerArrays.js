08188061ffa2fe572731fd42d05f5cfe0650c372
Naveen Sharma <naveencoder13@gmail.com>
2019-05-19 10:51:56 +0530

    Added square brackets in the variables of "Concat Two Integer Arrays" javascript algorithm file.

--- concatTwoIntegerArrays/concatTwoIntegerArrays.js
+++ concatTwoIntegerArrays/concatTwoIntegerArrays.js
@@ -1,6 +1,6 @@
 function cocatIntArr(arr1, arr2) {
     let concatenatedArr = arr1.concat(arr2);
-    console.log(`Before Concat: \nArray 1 - ${arr1} \nArray 2 - ${arr2} \n\nAfter Concat\n${concatenatedArr}.`);
+    console.log(`Before Concat: \nArray 1 - [${arr1}] \nArray 2 - [${arr2}] \n\nAfter Concat\n [${concatenatedArr}].`);
 }
 
 cocatIntArr([10, 100, 1000], [20, 200, 2000]);
